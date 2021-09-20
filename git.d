83a38439c47da4c2dd53ce7b53b572ddf1285e71
Nelson Sproul <nelson.sproul@gmail.com>
2021-09-20 10:16:57 -0700

    fixes

--- git.d
+++ git.d
@@ -1,2 +1,33 @@
 :
-git diff .|cat
+
+t=/tmp/git.d.latest.`pwd | sed -e 's/[^a-zA-Z0-9]/_/g'`
+
+all_mode=''
+dry_mode=''
+verbose_mode=''
+while [ -n "$1" ]; do
+        case "$1" in
+                a|-all)
+                        git.all $dry_mode git.d
+                        exit
+                ;;
+                -dry)
+                        dry_mode=-dry
+                ;;
+                -q|-quiet)
+                        verbose_mode=''
+                ;;
+                -v|-verbose)
+                        verbose_mode=-v
+                ;;
+                *)
+                        break
+                ;;
+        esac
+        shift
+done
+
+git diff . > $t
+strait $t
+exit
+bx $dp/git_util/git.d a
