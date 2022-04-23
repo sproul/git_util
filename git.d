8bf8c7bd2a7dfdbf5d9733a7e03fb85aeeea1a04
Nelson Sproul <nelson.sproul@oracle.com>
2022-04-22 21:19:44 -0700

    Synchronize_high_traffic_shared_projects_if_no_local_changes

--- git.d
+++ git.d
@@ -3,6 +3,7 @@ t=/tmp/git.d.latest.`pwd | sed -e 's/[^a-zA-Z0-9]/_/g'`
 
 all_mode=''
 dry_mode=''
+pull_latest_updates_mode=''
 verbose_mode=''
 while [ -n "$1" ]; do
         case "$1" in
@@ -25,6 +26,9 @@ while [ -n "$1" ]; do
                 -q|-quiet)
                         verbose_mode=''
                 ;;
+                -pull_latest_updates_if_no_local_changes)
+                        pull_latest_updates_mode=-pull_latest_updates
+                ;;
                 -v|-verbose)
                         verbose_mode=-v
                 ;;
@@ -41,9 +45,16 @@ strait $t
 git status --short > $t
 if [ -s "$t" ]; then
         git_util__modified_repos__Add_to_list `pwd`
+        rc=0
 else
         git_util__modified_repos__Remove_from_list `pwd`
+        if [ -n "$pull_latest_updates_mode" ]; then
+                echo Since no local changes, will go ahead and sync to the latest checkin for `pwd`...
+                git.up
+        fi
+        rc=1
 fi
 cat $t
+exit $rc
 exit
 bx $dp/git_util/git.d a
