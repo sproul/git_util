dbc8051b03cfd9e9c36ac971b907efe7340087c5
Nelson Sproul <nelson.sproul@gmail.com>
2024-08-09 20:44:46 -0700

    guard against .git/config disappearance on slc18utq

--- git.d
+++ git.d
@@ -1,56 +1,79 @@
 :
 t=/tmp/git.d.latest.`pwd | sed -e 's/[^a-zA-Z0-9]/_/g'`
 
+Detect_and_maybe_correct_weird_config_disappearance()
+{
+	if [ ! -f .git/config ]; then
+		echo "WARN confirmed no .git/config" 1>&2
+		possible_fallback=$HOME/git/$proj/.git/config
+		if [ -f $possible_fallback ]; then
+                        if cp $possible_fallback .git/config; then
+				echo "OK cp $possible_fallback .git/config" 1>&2
+                                return
+			else
+				echo "FAIL cp $possible_fallback .git/config" 1>&2
+                                exit 1
+                        fi
+                fi
+                echo "FAIL could not find a replacement for .git/config" 1>&2
+                exit 1
+	fi
+}
+
+
 all_mode=''
 dry_mode=''
 verbose_mode=''
 while [ -n "$1" ]; do
-        case "$1" in
-                a|-all)
-                        for d in `cat $dp/.modified_repo_statuses`; do
-                                git.d -in_dir $d
-                        done
-                        exit
-                ;;
-                -in_dir)
-                        shift
-                        dir="$1"
-                        if [ ! -d "$dir" ]; then
-                                echo "$0: error: could not find directory \"$dir\"" 1>&2
-                                exit 1
-                        fi
-                        cd "$dir"
-                ;;
-                -dry)
-                        dry_mode=-dry
-                ;;
-                -q|-quiet)
-                        verbose_mode=''
-                ;;
-                -v|-verbose)
-                        verbose_mode=-v
-                ;;
-                *)
-                        break
-                ;;
-        esac
-        shift
+	case "$1" in
+		a|-all)
+			for d in `cat $dp/.modified_repo_statuses`; do
+				git.d -in_dir $d
+			done
+			exit
+		;;
+		-in_dir)
+			shift
+			dir="$1"
+			if [ ! -d "$dir" ]; then
+				echo "$0: error: could not find directory \"$dir\"" 1>&2
+				exit 1
+			fi
+			cd "$dir"
+		;;
+		-dry)
+			dry_mode=-dry
+		;;
+		-q|-quiet)
+			verbose_mode=''
+		;;
+		-v|-verbose)
+			verbose_mode=-v
+		;;
+		*)
+			break
+		;;
+	esac
+	shift
 done
 
 case `pwd` in
         $HOME/git/qwickanalytics_semi_structured_data*)
                 echo "WARN difr, not asking git" 1>&2
-                #cd $(pwd | sed -e "s;$HOME;$dp;")
-                difr
-                exit 0
-        ;;
-        $dp/git/qwickanalytics_semi_structured_data*)
-                echo "WARN difr, not asking git" 1>&2
-                difr
-                exit 0
-        ;;
+		#cd $(pwd | sed -e "s;$HOME;$dp;")
+		difr
+		exit 0
+	;;
+	$dp/git/qwickanalytics_semi_structured_data*)
+		echo "WARN difr, not asking git" 1>&2
+		difr
+		exit 0
+	;;
 esac
 prune.midnight_files
+git_proj_dir=`ls.up -find_parent_of_dir .git`
+proj=`basename $git_proj_dir`
+Detect_and_maybe_correct_weird_config_disappearance
 git status --short > $t.mid
 
 git diff . > $t
@@ -58,13 +81,12 @@ strait $t
 
 . modified_repos.inc
 
-git_proj_dir=`ls.up -find_parent_of_dir .git`
 if [ -s "$t.mid" ]; then
-        git_util__modified_repos__Add_to_list $git_proj_dir
-        rc=0
+	git_util__modified_repos__Add_to_list $git_proj_dir
+	rc=0
 else
-        git_util__modified_repos__Remove_from_list $git_proj_dir
-        rc=1
+	git_util__modified_repos__Remove_from_list $git_proj_dir
+	rc=1
 fi
 rm  $t
 
